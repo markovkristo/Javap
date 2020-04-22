b1df9a5d0f60a04636f19b3983198b500aab07cb
Erik <erikkim06@gmail.com>
2020-04-22 14:26:58 +0300

    Panin eestikeelse nime

--- src/Menüü.java
+++ src/Menüü.java
@@ -1,6 +1,7 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import javafx.scene.image.ImageView;
@@ -9,21 +10,26 @@ import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-public class Menu  extends Application {
+public class Menüü  extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Pane root = new Pane();
         root.setPrefSize(800,600);
-        InputStream is = Files.newInputStream(Paths.get("taust.png"));
+        InputStream is = Files.newInputStream(Paths.get("C:\\Users\\Erik\\Desktop\\OOP_projekt_2\\src\\taust.png"));
         Image img = new Image(is);
         is.close();
 
-        ImageView imgView = new ImageView();
+        ImageView imgView = new ImageView(img);
+        imgView.setFitWidth(800);
+        imgView.setFitHeight(400);
 
         root.getChildren().addAll(imgView);
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+    public static class MenüüNupp extends StackPane{
+
     }
 
     public static void main(String[] args) {
