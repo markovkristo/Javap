0ce653f7fd8f3fb131d0153f30f883b4c84b5a21
Erik <erikkim06@gmail.com>
2020-04-22 13:55:44 +0300

    Heioo

---
+++ src/PeaKlass.java
@@ -0,0 +1,11 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+public class PeaKlass extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+    public void start(Stage theStage){
+        theStage.setTitle("Trips-Traps-Trull");
+        theStage.show();
+    }
+}
