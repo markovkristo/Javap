eb8e1b5faa4f0d267bee34212bb261fd744669b3
Erik <erikkim06@gmail.com>
2020-04-27 16:38:21 +0300

    Muudatus

--- src/Lõpuekraan.java
+++ src/Lõpuekraan.java
@@ -3,13 +3,20 @@ import javax.swing.*;
 public class Lõpuekraan {
     static void lõpp(char kumb) {
         Object[] valikud = {"Välju"};
-        if (kumb == 'X' || kumb == 'O') {
-            int valik = JOptionPane.showOptionDialog(null, kumb + " võitis!", "Tulemus",//Kui mäng on läbi, saab kasutaja valiku, kas mängida uuesti või väljuda mängust.
+        if (kumb == 'X') {
+            int valik = JOptionPane.showOptionDialog(null, Traps.esimene + " võitis!", "Tulemus",//Kui mäng on läbi, saab kasutaja valiku, kas mängida uuesti või väljuda mängust.
                     JOptionPane.YES_OPTION, JOptionPane.PLAIN_MESSAGE, null, valikud, null);
             if (valik == JOptionPane.YES_OPTION) {
                 System.exit(0);
             }
-        } else {
+        }
+        if (kumb == 'O') {
+            int valik = JOptionPane.showOptionDialog(null, Traps.esimene + " võitis!", "Tulemus",//Kui mäng on läbi, saab kasutaja valiku, kas mängida uuesti või väljuda mängust.
+                    JOptionPane.YES_OPTION, JOptionPane.PLAIN_MESSAGE, null, valikud, null);
+            if (valik == JOptionPane.YES_OPTION) {
+                System.exit(0);
+            }
+        }else {
             int valik = JOptionPane.showOptionDialog(null, "Viik!", "Tulemus",//Kui mäng on läbi, saab kasutaja valiku, kas mängida uuesti või väljuda mängust.
                     JOptionPane.YES_OPTION, JOptionPane.PLAIN_MESSAGE, null, valikud, null);
             if (valik == JOptionPane.YES_OPTION) {
