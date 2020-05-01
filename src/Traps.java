62d67ac9e68736750e86fd2a69475d046b493bab
Kristo Markov <markovkristo@gmail.com>
2020-05-01 13:31:36 +0300

    Kommentaarid

--- src/Traps.java
+++ src/Traps.java
@@ -17,15 +17,15 @@ import java.util.List;
 
 
 public class Traps extends Application {
-    private char praeguneMängija = 'X';
+    private char praeguneMängija = 'X'; // Kujund, kes alustab
     private Laud[][] ruut = new Laud[3][3];
-    private Label staatus = new Label("Käik on mängijal: " + esimene);
-    private List<Character> käigud = new ArrayList();
+    private Label staatus = new Label("Käik on mängijal: " + esimene); // Silt selleks, et stseeni all näidata kelle kord on.
+    private List<Character> käigud = new ArrayList(); // Järejend kuhu salvestatakse tehtud käigud.
     static String esimene = JOptionPane.showInputDialog("Esimese mängija nimi (X): "); //Laseb sisestada mängijate nimed,
     static String teine = JOptionPane.showInputDialog("Teise mängija nimi (O): ");//mis hiljem logisse kirja lähevad.
 
     @Override
-    public void start(Stage pealava) throws Exception {
+    public void start(Stage pealava) {
         reegel();
         GridPane pane = new GridPane();  // Kasutan Gridpane, et väljastada ruutude asendid.
 
@@ -82,20 +82,20 @@ public class Traps extends Application {
         public Laud() {
             setStyle("-fx-border-color: black");
             this.setPrefSize(600, 600);
-            this.setOnMouseClicked(e -> NupuVajutus());
+            this.setOnMouseClicked(e -> NupuVajutus()); // Hiireklahvi vajutusel läheb Nupuvajutus meetodisse.
 
         }
 
 
-        // Meetod, mis tegutseb siis kui toimub nupuvajutus.
+        // Meetod, mis tegutseb siis kui toimub hiireklahvi vajutus.
         private void NupuVajutus() {
-            if (käigud.contains(mängija)) {
+            if (käigud.contains(mängija)) { // Tingimuslause, mis kontrollib, kas antud ruudule on juba käik tehtud või ei ole.
                 staatus.setText("See koht on võetud käi mujale.");
                 return;
             }
             setMängija(praeguneMängija);
             if (kasVõitis(praeguneMängija)) { // Kontrollib, kas praegu mängija oma kõiguga võitis või ei
-                if (Character.valueOf(praeguneMängija) == 'X') {//Kui võitja on esimene mängija, salvestab selle logisse.
+                if (Character.valueOf(praeguneMängija) == 'X') { //Kui võitja on esimene mängija, salvestab selle logisse.
                     staatus.setText(esimene + " võitis!");
                     Logitamine.logitamine(esimene, 1);
                     Lõpuekraan.lõpp(praeguneMängija);
@@ -105,7 +105,7 @@ public class Traps extends Application {
                     Lõpuekraan.lõpp(praeguneMängija);
                 }
 
-            } else if (kasLaudOnTäis()) { // Kontrollib, kas mängulaud on täis.
+            } else if (kasLaudOnTäis()) { // Kontrollib, kas mängulaud on täis ja kui on, siis muudetakse praegune mängja ära ning kuvatakse vastav lõpuekraan.
                 staatus.setText("Viik!");
                 praeguneMängija = ' ';
                 Lõpuekraan.lõpp(praeguneMängija);
@@ -121,38 +121,39 @@ public class Traps extends Application {
             return mängija;
         }
 
-        public void setMängija(char x) { // Meetod, mis joonistab vastava mängija käigu mängulauale.
+        public void setMängija(char x) { // Meetod, mis joonistab vastava mängija käigu mängulauale ning lisab tehtud käigu käikude järjendisse.
             mängija = x;
             if (mängija == 'X') {
-                Line joon1 = new Line(10, 10, this.getWidth() - 10, this.getHeight() - 10);
+                Line joon1 = new Line(10, 10, this.getWidth() - 10, this.getHeight() - 10); // X-i üks joon
                 joon1.endXProperty().bind(this.widthProperty().subtract(10));
                 joon1.endYProperty().bind(this.heightProperty().subtract(10));
-                joon1.setStrokeWidth(10.3);
+                joon1.setStrokeWidth(10.3); // Määran esimese joone laiuse ja värvi
                 joon1.setStroke(Color.HOTPINK);
-                Line joon2 = new Line(10, this.getHeight() - 10, this.getWidth() - 10, 10);
+                Line joon2 = new Line(10, this.getHeight() - 10, this.getWidth() - 10, 10); // X-i teine joon
                 joon2.endXProperty().bind(this.widthProperty().subtract(10));
                 joon2.startYProperty().bind(this.heightProperty().subtract(10));
-                joon2.setStrokeWidth(10.3);
+                joon2.setStrokeWidth(10.3); // Määran teise joone laiuse ja värvi
                 joon2.setStroke(Color.DODGERBLUE);
-                Logitamine.logitamine(esimene, 0);
+                Logitamine.logitamine(esimene, 0); // Lisan nende joonistamise logifaili.
                 getChildren().addAll(joon1, joon2);
-                käigud.add(mängija);
+                käigud.add(mängija); // Lisan tehtud käigu käikude järjendisse.
             } else if (mängija == 'O') {
                 Ellipse ring = new Ellipse(this.getWidth() / 2, this.getHeight() / 2, this.getWidth() / 2 - 10, this.getHeight() / 2 - 10);
                 ring.centerXProperty().bind(this.widthProperty().divide(2));
                 ring.centerYProperty().bind(this.heightProperty().divide(2));
                 ring.radiusXProperty().bind(this.widthProperty().divide(2).subtract(10));
                 ring.radiusYProperty().bind(this.heightProperty().divide(2).subtract(10));
-                ring.setStroke(Color.DODGERBLUE);
-                ring.setStrokeWidth(5.0);
-                ring.setFill(Color.HOTPINK);
-                Logitamine.logitamine(teine, 0);
+                ring.setStroke(Color.DODGERBLUE); // Ringi värv.
+                ring.setStrokeWidth(5.0); // Ringi välisjoone laius.
+                ring.setFill(Color.HOTPINK); // Seespoolsema ringi värv.
+                Logitamine.logitamine(teine, 0);  // Ringi joonistamise lisamine logifaili.
                 getChildren().add(ring);
-                käigud.add(mängija);
+                käigud.add(mängija); // Lisan tehtud käigu käikude järjendisse.
             }
         }
 
     }
+
     public static void reegel(){
         Platform.runLater(new Runnable() {
             @Override
@@ -161,7 +162,6 @@ public class Traps extends Application {
             }
         });
     }
-
     public static void main(String[] args) {
         launch(args);
     }
